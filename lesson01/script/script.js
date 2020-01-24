cd7b216712015fda09e17345bb1c67fc3f3eeff7
Ckopn84 <ivann@bk.ru>
2020-01-25 01:02:01 +0400

    Lesson01
    Основное задание в дополнительной ветке

---
+++ lesson01/script/script.js
@@ -0,0 +1,10 @@
+const money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert('3) Вывод в модальном окне сообщения с любым текстом =)');
+
+console.log('Вывод в консоль сообщения с любым текстом, например: "Привет, МИР!"');
