92c7da14a24a52629b2233e12b5127c8adac3d71
Ckopn84 <ivann@bk.ru>
2020-02-19 01:27:15 +0400

    Add task lesson17

--- lesson17/js/script.js
+++ lesson17/js/script.js
@@ -27,14 +27,20 @@ window.addEventListener('DOMContentLoaded', () => {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSecunds = document.querySelector('#timer-seconds');
+        let idInterval = 0;
 
         const getTimeRemaining = () => {
             const dateStop = new Date(dedline).getTime(),
                 dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60),
-                minutes = Math.floor((timeRemaining / 60) % 60),
+                timeRemaining = (dateStop - dateNow) / 1000;
+            let seconds = 0,
+                minutes = 0,
+                hours = 0;
+            if (timeRemaining > 0) {
+                seconds = Math.floor(timeRemaining % 60);
+                minutes = Math.floor((timeRemaining / 60) % 60);
                 hours = Math.floor(timeRemaining / 60 / 60);
+            }
             /* return {
                 'hours': hours,
                 'minutes': minutes,
@@ -48,7 +54,7 @@ window.addEventListener('DOMContentLoaded', () => {
             };
         };
 
-        const updateClock = () => {
+        /* const updateClock = () => {
             let timer = getTimeRemaining();
 
             timerHours.textContent = timer.hours;
@@ -60,8 +66,28 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         };
 
-        updateClock();
+        updateClock(); */
+
+        const addZero = elem => {
+            if (String(elem).length === 1) { return '0' + elem; } else { return String(elem); }
+        };
+
+        const updateClock = () => {
+            let timer = getTimeRemaining();
+            timerHours.textContent = addZero(timer.hours);
+            timerMinutes.textContent = addZero(timer.minutes);
+            timerSecunds.textContent = addZero(timer.seconds);
+
+            console.log('timer.timeRemaining: ', timer.timeRemaining);
+            if (timer.timeRemaining < 0) {
+                clearInterval(idInterval);
+            }
+            console.log('idInterval: ', idInterval);
+        };
+
+        idInterval = setInterval(updateClock, 1000);
     };
 
-    countTimer('20 Feb 2020');
+    // countTimer('19 Feb 2020');
+    countTimer('21 Feb 2020');
 });
