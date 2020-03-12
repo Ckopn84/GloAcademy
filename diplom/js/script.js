ab3378f11d5b4973ea039b9fbe8d875c34dd929d
Ckopn84 <ivann@bk.ru>
2020-03-12 18:56:48 +0400

    add init popup call my

--- diplom/js/script.js
+++ diplom/js/script.js
@@ -2,4 +2,37 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 	//
+	const togglePopUp = () => {
+		const showPopup = elem => elem.style.display = 'block';
+
+		const closePopup = elem => elem.style.display = 'none';
+
+		const initPopup = classPopup => {
+			const popup = document.querySelector(classPopup),
+				callBtn = document.querySelector('.contacts .call-btn');
+
+			callBtn.addEventListener('click', () => showPopup(popup));
+
+			popup.addEventListener('click', event => {
+				let target = event.target;
+
+				if (target.classList.contains('popup-close')) {
+					closePopup(popup);
+				} else {
+					target = target.closest('.popup-content');
+
+					if (!target) {
+						closePopup(popup);
+					}
+				}
+			});
+		};
+
+		document.body.addEventListener('click', event => {
+			const target = event.target;
+
+			if (target.closest('.contacts') && target.classList.contains('call-btn')) initPopup('.popup-call');
+		});
+	};
+	togglePopUp();
 });
