62882b14954e1aa15fe657c6412eb3a43f96187e
Ckopn84 <ivann@bk.ru>
2020-03-13 14:47:35 +0400

    add accordion

--- diplom/src/index.js
+++ diplom/src/index.js
@@ -2,6 +2,8 @@
 
 import togglePopUp from './modules/togglePopUp';
 import showSentenceBlock from './modules/showSentenceBlock';
+import toggleAccordion from './modules/toggleAccordion';
 
 togglePopUp();
 showSentenceBlock();
+toggleAccordion();
---
+++ diplom/src/modules/toggleAccordion.js
@@ -0,0 +1,41 @@
+
+const toggleAccordion = () => {
+	const handlingPanel = idElem => {
+		const elem = document.querySelector(idElem),
+			accordion = elem.closest('.panel-group'),
+			parent = elem.closest('.panel-default');
+
+		const togglePanel = (elem, option = '') => {
+			if (!!option) {
+				if (!elem.children[1].classList.contains('in'))
+					elem.children[1].classList.add('in');
+			} else if (elem.children[1].classList.contains('in'))
+				elem.children[1].classList.remove('in');
+		};
+
+		for (const item of accordion.children) {
+			if (item === parent) {
+				togglePanel(item, 'in');
+			} else {
+				togglePanel(item);
+			}
+		}
+};
+
+	document.body.addEventListener('click', event => {
+		let target = event.target;
+
+		if ((target.closest('#accordion') || target.closest('#accordion-two')) &&
+			(target.closest('.panel-heading') ||
+				target.tagName.toLowerCase() === 'a' ||
+				target.parentElement.tagName.toLowerCase() === 'a')) {
+			const hash = target.closest('[href^="#"]') ?
+				target.closest('[href^="#"]').href.replace(/[^#]+(.*)/, '$1') :
+				target.querySelector('a').href.replace(/[^#]+(.*)/, '$1');
+
+			handlingPanel(hash);
+		}
+	});
+};
+
+export default toggleAccordion;
