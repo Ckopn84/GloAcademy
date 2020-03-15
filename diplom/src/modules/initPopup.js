a7bf95ec57b6c477a767e6b3d4b088853a0599f8
Ckopn84 <ivann@bk.ru>
2020-03-16 01:12:25 +0400

    selected closePopup in module

--- diplom/src/modules/initPopup.js
+++ diplom/src/modules/initPopup.js
@@ -1,29 +1,18 @@
 
+import closePopup from './closePopup';
+
 const initPopup = (classPopup) => {
 	const popup = document.querySelector(classPopup);
 
 	const showPopup = elem => elem.style.display = 'block';
-
-	const slearInputs = elem => {
-		const inputs = elem.querySelectorAll('input');
-
-		inputs.forEach(item => {
-			if (item.type !== 'button') item.value = '';
-		});
-	};
-
-	const closePopup = elem => {
-		elem.style.display = 'none';
-
-		slearInputs(elem);
-	};
 
 	showPopup(popup);
 
 	popup.addEventListener('click', event => {
 		let target = event.target;
 
-		if (target.classList.contains('popup-close')) {
+		if (target.classList.contains('popup-close') ||
+			target.classList.contains('close-popup')) {
 			event.preventDefault();
 			closePopup(popup);
 		} else {
